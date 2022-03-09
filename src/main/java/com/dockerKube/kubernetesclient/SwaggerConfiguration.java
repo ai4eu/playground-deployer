5f7b4031d08759bee5f8eb2edf6c09c86530ad4f
mwelss-fhg <martin.welss@iais.fraunhofer.de>
2022-03-09 13:07:55 +0100

    fix swagger config

--- src/main/java/com/dockerKube/kubernetesclient/SwaggerConfiguration.java
+++ src/main/java/com/dockerKube/kubernetesclient/SwaggerConfiguration.java
@@ -32,11 +32,11 @@ import springfox.documentation.service.ApiInfo;
 import springfox.documentation.service.Contact;
 import springfox.documentation.spi.DocumentationType;
 import springfox.documentation.spring.web.plugins.Docket;
-import springfox.documentation.swagger2.annotations.EnableSwagger2;
+
+import java.util.ArrayList;
 
 
 @Configuration
-@EnableSwagger2
 public class SwaggerConfiguration {
 	@Bean
 	public Docket api() {
@@ -55,7 +55,7 @@ public class SwaggerConfiguration {
 				"Terms of service",
 				new Contact("Eclipse Graphene Team", "", ""),
 				"Apache 2.0",
-				"https://www.apache.org/licenses/LICENSE-2.0", null);
+				"https://www.apache.org/licenses/LICENSE-2.0", new ArrayList<>());
 
 		return apiInfo;
 	}
