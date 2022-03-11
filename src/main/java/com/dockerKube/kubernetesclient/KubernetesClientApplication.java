71def2708afe3e81add20da7d8a2643dbe19350f
mwelss-fhg <martin.welss@iais.fraunhofer.de>
2022-03-11 10:57:17 +0100

    fix version infos

--- src/main/java/com/dockerKube/kubernetesclient/KubernetesClientApplication.java
+++ src/main/java/com/dockerKube/kubernetesclient/KubernetesClientApplication.java
@@ -19,29 +19,40 @@
  */
 package com.dockerKube.kubernetesclient;
 
+import org.slf4j.Logger;
+import org.slf4j.LoggerFactory;
 import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
 import org.springframework.context.annotation.ComponentScan;
 
 import com.fasterxml.jackson.databind.ObjectMapper;
 
+import java.util.Properties;
+
 @SpringBootApplication
 @ComponentScan(basePackages = {"com.dockerKube.*"})
 public class KubernetesClientApplication {
 	public static final String CONFIG_ENV_VAR_NAME = "SPRING_APPLICATION_JSON";
+	public static Logger logger = LoggerFactory.getLogger("init");
 	public static void main(String[] args)throws Exception {
 		final String springApplicationJson = System.getenv(CONFIG_ENV_VAR_NAME);
 		if (springApplicationJson != null && springApplicationJson.contains("{")) {
 			final ObjectMapper mapper = new ObjectMapper();
 			// ensure it's valid
 			mapper.readTree(springApplicationJson);
-			// logger.info("main: successfully parsed configuration from environment {}",
-			// CONFIG_ENV_VAR_NAME);
+			logger.info("main: successfully parsed configuration from environment {}", CONFIG_ENV_VAR_NAME);
 		} else {
-			// logger.warn("main: no configuration found in environment {}",
-			// CONFIG_ENV_VAR_NAME);
+			logger.info("main: no configuration found in environment {}", CONFIG_ENV_VAR_NAME);
 		}
-		
+		try {
+			Properties manifest = new Properties();
+			manifest.load(KubernetesClientApplication.class.getResourceAsStream("/META-INF/MANIFEST.MF"));
+			manifest.entrySet().stream().forEach(e -> logger.info(e.toString()));
+		} catch(Exception x) {
+			logger.warn("no version information");
+			x.printStackTrace();
+		}
+
 		SpringApplication.run(KubernetesClientApplication.class, args);
 	}
 }
